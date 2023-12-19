9e7fa552170b02427d13c18acc8aaa389418e896
VioleGracex <highdevilxx@hotmail.com>
2023-12-19 15:28:56 +0500

    2.3

--- ScreamJam2023/Assets/_Project/Scirpts/CassettePlayer/Cass_Player.cs
+++ ScreamJam2023/Assets/_Project/Scirpts/CassettePlayer/Cass_Player.cs
@@ -1,86 +1,177 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Timeline;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class Cass_Player : MonoBehaviour
 {
     public PlayableDirector activePlayable;
     public int activeTape = 0;
-    public List<int> tapes = new List<int> { 0 };
-    [SerializeField] List<TimelineAsset> tapeClips;
+    [SerializeField] TextMeshProUGUI activeTapeName;
+    public List<int> tapes_Owned = new List<int> { 0 };
+    [SerializeField] List<PlayableDirector> tapeClips;
     public ToggleGroup functionToggleGroup; // Reference to the ToggleGroup for play, stop, pause, rewind
+
+    [SerializeField] Canvas CassPlayer_UI;
+
+    private Toggle playToggle;
+    private Toggle stopToggle;
+    private Toggle pauseToggle;
+    private Toggle rewindToggle;
+    private Toggle nextToggle;
+
+    bool switchedTape = false;
+    private PlayerControls controls;
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+
+        controls.Cass_Player_Input.Next.performed += ctx => NextTape();
+        controls.Cass_Player_Input.Play.performed += ctx => Play();
+        controls.Cass_Player_Input.Stop.performed += ctx => Stop();
+        controls.Cass_Player_Input.Pause.performed += ctx => Pause();
+        controls.Cass_Player_Input.Rewind.performed += ctx => RewindTape();
+
+        // Initialize toggles
+        playToggle = FindToggleInChildren("PlayToggle");
+        stopToggle = FindToggleInChildren("StopToggle");
+        pauseToggle = FindToggleInChildren("PauseToggle");
+        rewindToggle = FindToggleInChildren("RewindToggle");
+        nextToggle = FindToggleInChildren("NextToggle");
+    }
+
+    private void OnEnable()
+    {
+        controls.Cass_Player_Input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Cass_Player_Input.Disable();
+    }
 
     private void Start()
     {
         // Ensure activeTape is within bounds
-        activeTape = Mathf.Clamp(activeTape, 0, tapes.Count - 1);
+        activeTape = Mathf.Clamp(activeTape, 0, tapes_Owned.Count - 1);
         // Initialize the activePlayable with the first tape (if available)
         if (tapeClips.Count > 0)
-            activePlayable.playableAsset = tapeClips[tapes[activeTape]];
+            activePlayable = tapeClips[tapes_Owned[activeTape]];
     }
 
     public void AddTape(int num)
     {
-        tapes.Add(num);
+        tapes_Owned.Add(num);
+
+        if (activeTape == 0)
+        {
+            AssignActiveTape(num);
+            Debug.Log("Added tape num: " + num);
+        }
     }
 
     public void AssignActiveTape(int num)
     {
-        activeTape = Mathf.Clamp(num, 0, tapes.Count - 1);
+        activeTape = Mathf.Clamp(num, 0, tapes_Owned.Count - 1);
         // Change the active playable to the assigned tape
-        activePlayable.playableAsset = tapeClips[tapes[activeTape]];
+        activePlayable = tapeClips[tapes_Owned[activeTape]];
     }
 
     public void NextTape()
     {
-        if (tapes.Count == 0)
+        if (nextToggle != null)
         {
-            Debug.LogWarning("No tapes available.");
+            nextToggle.isOn = true;
+            StartCoroutine(TurnOffToggleAfterDelay(nextToggle, 2f));
+        }
+        else
+        {
+            Debug.LogError("NextToggle not found.");
+        }
+
+        if(activePlayable == null)
+            return;
+        if (tapes_Owned.Count <= 1)
+        {
+            Debug.LogWarning("No additional tapes available.");
             return;
         }
 
-        activeTape = (activeTape + 1) % tapes.Count;
+        activePlayable.Stop();
+
+        int indexOfActiveTape = tapes_Owned.IndexOf(activeTape);
+        int nextTapeIndex = (indexOfActiveTape + 1) % tapes_Owned.Count;
+
+        if (nextTapeIndex == indexOfActiveTape)
+        {
+            Debug.LogWarning("No different next tape available.");
+            return;
+        }
+
         // Change the active playable to the next tape
-        activePlayable.playableAsset = tapeClips[tapes[activeTape]];
-        UpdateFunctionToggle();
+        activeTape = nextTapeIndex;
+        activePlayable = tapeClips[tapes_Owned[activeTape]];
+        activeTapeName.text = activePlayable.gameObject.name;
+        switchedTape = true;
+        Debug.Log("Switched tapes");
+
+        // Set the "NextToggle" and turn it off after 2 seconds
+        
     }
 
     public void Play()
     {
+        if(activePlayable == null)
+            return;
         activePlayable.Play();
-        UpdateFunctionToggle();
+        playToggle.isOn = true;
+        //UpdateFunctionToggle();
     }
 
     public void Stop()
-    {
-        activePlayable.Stop();
-        UpdateFunctionToggle();
+    {   
+        if(activePlayable == null)
+            return;
+        activePlayable.Pause();
+        stopToggle.isOn = true;
+        StartCoroutine(TurnOffToggleAfterDelay(stopToggle, 2f));
     }
 
     public void Pause()
     {
-        activePlayable.Pause();
-        UpdateFunctionToggle();
+        if(activePlayable == null)
+            return;
+        activePlayable.Stop();
+        pauseToggle.isOn = true;
     }
 
     public void RewindTape()
     {
+        if(activePlayable == null)
+            return;
         // Check if the activePlayable is valid
         if (activePlayable.playableAsset != null)
         {
             // Calculate the new time by subtracting a certain duration (e.g., 1 second)
-            double newTime = activePlayable.time - 1.0; // Adjust the rewind duration as needed
+            double newTime = activePlayable.time - 2.0; // Adjust the rewind duration as needed
 
             // Clamp the new time to ensure it doesn't go below 0
             newTime = Mathf.Clamp((float)newTime, 0f, (float)activePlayable.duration);
 
             // Set the new time for the activePlayable
             activePlayable.time = newTime;
-            activePlayable.Play(); // Play from the new time
-            UpdateFunctionToggle();
+
+            // Set the speed to make it play backward
+            activePlayable.playableGraph.GetRootPlayable(0).SetSpeed(-1.0f);
+
+            // Play from the new time
+            activePlayable.Play();
         }
         else
         {
@@ -88,40 +179,17 @@ public class Cass_Player : MonoBehaviour
         }
     }
 
-    private void UpdateFunctionToggle()
+
+    private IEnumerator TurnOffToggleAfterDelay(Toggle toggle, float delay)
     {
-        // Determine the active function based on the state of Play, Stop, Pause, Rewind
-        Toggle activeToggle = null;
-
-        if (activePlayable.state == PlayState.Playing)
-        {
-            activeToggle = functionToggleGroup.ActiveToggles().FirstOrDefault(toggle => toggle.name == "PlayToggle");
-        }
-        else if (activePlayable.state == PlayState.Paused)
-        {
-            activeToggle = functionToggleGroup.ActiveToggles().FirstOrDefault(toggle => toggle.name == "PauseToggle");
-        }
-        else
-        {
-            if (activePlayable.time > 0)
-            {
-                activeToggle = functionToggleGroup.ActiveToggles().FirstOrDefault(toggle => toggle.name == "RewindToggle");
-            }
-            else
-            {
-                activeToggle = functionToggleGroup.ActiveToggles().FirstOrDefault(toggle => toggle.name == "StopToggle");
-            }
-        }
-
-        // Toggle on the active toggle and toggle off others
-        if (activeToggle != null)
-        {
-            activeToggle.isOn = true;
-        }
-        else
-        {
-            functionToggleGroup.SetAllTogglesOff();
-        }
+        switchedTape = false;
+        yield return new WaitForSeconds(delay);
+        toggle.isOn = false;
     }
 
+    private Toggle FindToggleInChildren(string toggleName)
+    {
+        Toggle[] toggles = GetComponentsInChildren<Toggle>(true); // Include inactive toggles
+        return toggles.FirstOrDefault(toggle => toggle.name == toggleName);
+    }
 }
